2c74ca8003f3eccd52b011482f557aaaa7d41931
Eloy García Almadén <eloy.garcia.pca@gmail.com>
2019-09-16 21:36:54 +0200

    Adding setup script for installation

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kayordomo",
+    version="1.0",
+    author="Eloy García Almadén",
+    author_email="eloy.garcia.pca@gmail.com",
+    description="This is a very small web service to interact with Kodi locally. When it is up and running you can tell kodi's addons to do elaborated things via JSONRPC Kodi's protocol",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/egara/kayordomo",
+    packages=setuptools.find_packages(),
+    install_requires=[
+       'unidecode>=1.1.1',
+       'requests>=2.22.0',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: POSIX :: Linux",
+        "Development Status :: 4 - Beta",
+	"Environment :: Web Environment",
+        "Intended Audience :: End Users/Desktop",
+	"Topic :: Internet :: WWW/HTTP :: HTTP Servers",
+        "Topic :: Utilities"
+    ],
+)
